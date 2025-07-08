2e6bf35e97dc7871dd47bc53402174ec98f6357d
Ss0Mae <ssomae@protonmail.ch>
2025-07-08 18:26:57 +0900

    Changes

---
+++ 1st_week/01_04_find_max_plus_or_multiply.py
@@ -0,0 +1,15 @@
+def find_max_plus_or_multiply(array):
+    # 이 부분을 채워보세요!
+    res = 0
+    for num in array:
+        if num <= 1 or res <=1:
+            res+=num
+        else:
+            res *=num
+    return res
+
+
+result = find_max_plus_or_multiply
+print("정답 = 728 현재 풀이 값 =", result([0,3,5,6,1,2,4]))
+print("정답 = 8820 현재 풀이 값 =", result([3,2,1,5,9,7,4]))
+print("정답 = 270 현재 풀이 값 =", result([1,1,1,3,3,2,5]))
